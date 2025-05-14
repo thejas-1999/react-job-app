4d1ef7f97e2eba46a879fb6515738618ce7f364a
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-05-14 11:09:56 +0530

    initialize tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 class="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 export default App;
